ae1d6a53aee1e41bdcf7bea52ba06370374c5d9c
Nigel Greenway <NigelGreenway@users.noreply.github.com>
2016-11-15 14:39:32 +0000

    Add: Extra functionality to ease usage of Docker

--- dkru.utility.bash
+++ dkru.utility.bash
@@ -9,7 +9,7 @@ things like `docker stop $(docker ps -aq)`.
 '
 dkru() {
   local OPTIND
-  getopts ":lrudh" ACTION
+  getopts ":lsrudh" ACTION
   getopts ":ci" OPTION
 
   case $ACTION in
@@ -31,6 +31,13 @@ dkru() {
           echo "pass -c for containers only or -i for images only"
           ;;
       esac
+      ;;
+    s)
+      echo "Container raw status:"
+      docker ps -a
+      echo ""
+      echo "Image raw status"
+      docker images -a
       ;;
     r)
       shift
@@ -71,16 +78,22 @@ dkru() {
       ;;
   esac
 
+
+  #############
+  ## Functions
+  #############
+
   __='
   Remove all or specified Docker containers
   '
   __detach_containers() {
+
     if [[ "$#" -gt 0 ]]
     then
       for CONTAINER in $@
       do
         ## Check if the container is running
-        if __is_container_running $CONTAINER
+        if [[ $(__is_container_running $CONTAINER) -gt 0 ]]
         then
           echo "$CONTAINER is running, please stop the container first!"
           return
@@ -88,16 +101,25 @@ dkru() {
         docker rm "$CONTAINER"
       done
     else
-      ## Check if any containers are still running
-      # docker rm $(docker -aq)
-      echo 'Ooo'
+      echo -n "Are you sure you want to remove all containers? [y/N]"
+      read response
+      case $response in
+        [yY][eE][sS]|[yY] )
+          docker rm $(docker ps -aq)
+          ;;
+        * )
+          false
+          ;;
+      esac
     fi
+
   }
 
   __='
   Remove all or specified Docker images
   '
   __detach_images() {
+
     if [[ "$#" -gt 0 ]]
     then
       for IMAGE in $@
@@ -105,50 +127,78 @@ dkru() {
           docker rmi "$IMAGE"
       done
     else
+      echo -n "Are you sure you want to remove all images? [y/N]"
+      read response
+      case $response in
+        [yY][eE][sS]|[yY] )
+          docker rmi $(docker images -aq)
+          ;;
+        * )
+          false
+          ;;
+      esac
       docker rmi $(docker images -aq)
     fi
+
   }
 
   __='
   Start containers
   '
   __start_containers() {
+
+    CONTAINER=$1  
     if [[ -f $(which docker-compose) && -f 'docker-compose.yml' ]]
     then
       docker-compose up
     elif [[ -f $(which docker) ]]
     then
-      echo 'docker start containers!'
+      __is_container_running "$CONTAINER"
+      if [[ $(__is_container_running "$CONTAINER") -gt 0 ]]
+      then
+        echo "$CONTAINER is running already"
+      else
+        docker start $(docker ps -f STATUS=exited | grep "$CONTAINER" | awk '{ print $1 }')
+      fi
     else
       echo "You don't seem to have `docker-compose` or the Docker engine installed?"
     fi
+
   }
 
   __='
   Stop containers
   '
   __stop_containers() {
+
     if [[ -f $(which docker-compose) && -f 'docker-compose.yml' ]]
     then
       docker-compose stop
     elif [[ -f $(which docker) ]]
     then
-      echo 'docker start containers!'
+
+      if [[ "$#" -gt 0 ]]
+      then
+        for CONTAINER in $@
+        do
+          echo "Stopping $CONTAINER"
+          docker stop "$CONTAINER"
+        done
+        clear
+      else
+        docker stop $(docker ps -aq)
+      fi
+
     else
       echo "You don't seem to have `docker-compose` or the Docker engine installed?"
     fi      
+
   }
 
   __='
   Check if a container is in a running state
   '
   __is_container_running() {
-    CONTAINER=$1
-    if docker ps -af STATUS=running | grep -q "$CONTAINER"
-    then
-      return 0
-    fi
-
-    return 1
+    docker ps -af STATUS=running | grep -c "$1"
   }
 }
